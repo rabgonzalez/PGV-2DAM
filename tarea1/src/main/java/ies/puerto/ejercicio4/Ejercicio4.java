eeb99656f18de03999c768d6ea080971c35d2497
rabgonzalez <rubalba.rag@gmail.com>
2024-09-24 10:10:12 +0100

    feat(tarea1): ejercicio 4

--- tarea1/src/main/java/ies/puerto/ejercicio4/Ejercicio4.java
+++ tarea1/src/main/java/ies/puerto/ejercicio4/Ejercicio4.java
@@ -1,10 +1,22 @@
 package ies.puerto.ejercicio4;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Ejercicio4 {
     public Ejercicio4() {}
 
     public static void main(String[] args) {
-        ProcessBuilder pb = new ProcessBuilder("java", "-cp", "./tarea1/target/classes", "ies.puerto.ejercicio.ClaseB", "hola mundo!");
-        
+        ProcessBuilder pb = new ProcessBuilder("echo", "Hola mundo! este es el Ejercicio 4");
+        pb.redirectErrorStream(true);
+        pb.redirectOutput(new File("tarea1/src/main/resources/ejercicio4.txt"));
+
+        try {
+            Process proceso = pb.start();
+            int codigo = proceso.waitFor();
+            System.out.println("El código es : "+codigo);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
