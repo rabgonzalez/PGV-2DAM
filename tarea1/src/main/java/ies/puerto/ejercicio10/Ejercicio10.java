370136eef743e5cfd46f1a5b672340712ddced44
rabgonzalez <rubalba.rag@gmail.com>
2024-09-25 16:34:38 +0100

    feat(tarea1): no sabemos hacer el ni 10 ni el 7 (el ejemplo es cat, por eso funciona)

--- tarea1/src/main/java/ies/puerto/ejercicio10/Ejercicio10.java
+++ tarea1/src/main/java/ies/puerto/ejercicio10/Ejercicio10.java
@@ -1,28 +1,43 @@
 package ies.puerto.ejercicio10;
 
 import java.io.IOException;
-import java.io.File;
-import java.io.DataInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 public class Ejercicio10 {
     public Ejercicio10(){}
 
     public static void main(String[] args) {
         ProcessBuilder pb = new ProcessBuilder("ls");
-        ProcessBuilder pb2 = new ProcessBuilder("grep");
-        pb.redirectErrorStream(true);
-        pb2.redirectErrorStream(true);
+        //ProcessBuilder pb2 = new ProcessBuilder("grep", "tarea1");
 
         try {
-            Process p = pb.start();
-            Process p2 = pb2.start();
-            InputStream entrada = p.getInputStream();
-            OutputStream salida = p.getOutputStream();
-            p.waitFor();
-            p2.waitFor();
-        } catch (IOException | InterruptedException e) {
+            Process process = pb.start();
+            //Process process2 = pb2.start();
+
+            OutputStream output = process.getOutputStream();
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(output));
+            
+            bw.write("tarea10");
+            bw.newLine();
+            bw.flush();
+            bw.close();
+
+            InputStream input = process.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(input));
+            
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            br.close();
+            //System.out.println(process2.waitFor());
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
